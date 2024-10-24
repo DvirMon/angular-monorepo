c2e4a912c4e1096db9c9b79632d1ec473552679c
dvir.monajem <dmenajem@gmail.com>
2024-10-24 13:17:51 +0300

    ✨ feat(user.service): Save User
    add post request for saving user to db

--- apps/travel-on/src/app/auth/utils/user.service.ts
+++ apps/travel-on/src/app/auth/utils/user.service.ts
@@ -2,14 +2,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { User } from './auth.model';
 import { API_URL } from '../../shared/tokens';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-
   readonly #http = inject(HttpClient);
 
   readonly #apiUrl = inject(API_URL);
-
 
   public getUser(idToken: string) {
     const headers = new HttpHeaders({
@@ -19,5 +18,7 @@ export class UserService {
     return this.#http.get<User>(`${this.#apiUrl}/users/profile`, { headers });
   }
 
-  
+  public saveUser(user: User): Observable<User> {
+    return this.#http.post<User>(`${this.#apiUrl}/users`, { user });
+  }
 }
