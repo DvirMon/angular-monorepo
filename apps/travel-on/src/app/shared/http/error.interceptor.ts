4727999d9aa7710d952e9f5003435b739c4b2953
dvir.monajem <dmenajem@gmail.com>
2024-10-24 10:05:23 +0300

    ✨ feat(interceptor): Add Logic
    add logic to catch connection network error

--- apps/travel-on/src/app/shared/http/error.interceptor.ts
+++ apps/travel-on/src/app/shared/http/error.interceptor.ts
@@ -3,10 +3,21 @@ import { catchError, throwError } from 'rxjs';
 
 export const errorInterceptor: HttpInterceptorFn = (req, handle) => {
   return handle(req).pipe(
-    catchError((err: HttpErrorResponse) => {
+    catchError((error: HttpErrorResponse) => {
+      if (!navigator.onLine) {
+        // Check if it's a connection error (offline)
+        alert('You are offline. Please check your internet connection.');
+      } else if (error.status === 0) {
+        // This indicates a network connection error
+        alert('Connection error! Please try again later.');
+      } else {
+        // Handle other types of errors
+        console.error('Error Status:', error.status);
+        console.error('Error Details:', error.message);
+      }
+      // Throw the error to be handled elsewhere if needed
 
-      return throwError(() => err);
+      return throwError(() => error);
     })
   );
 };
-
