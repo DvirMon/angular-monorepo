43c062f5b19a1628fabdf2ce22f3df9a9e541b91
dvir.monajem <dmenajem@gmail.com>
2024-10-24 14:27:12 +0300

    remove firebase config

--- apps/travel-on/src/app/app.config.ts
+++ apps/travel-on/src/app/app.config.ts
@@ -1,17 +1,13 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import {
   ApplicationConfig,
-  importProvidersFrom,
-  provideExperimentalZonelessChangeDetection,
+  provideExperimentalZonelessChangeDetection
 } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { appRoutes } from './app.routes';
 import { provideErrorService } from './shared/error.service';
 import { errorInterceptor } from './shared/http/error.interceptor';
-import { provideZonelessFirebase } from './shared/providers/zoneless-firebase';
-import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,11 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(appRoutes, withComponentInputBinding()),
     provideHttpClient(withInterceptors([errorInterceptor])),
     provideAnimations(),
-    // provideZonelessFirebase({
-    //   options: environment.firebaseConfig,
-    //   features: { auth: { emulators: { host: 'localhost', port: 9099 } } },
-    // }),
     provideErrorService(),
-    importProvidersFrom(NgxSkeletonLoaderModule),
   ],
 };
